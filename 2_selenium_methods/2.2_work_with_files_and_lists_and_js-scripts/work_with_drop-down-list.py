606df7ddeb680dfdcdb02a011c801e0bd78cef76
Vladislav Bolshakov <bolshakov.vladislav@gmail.com>
2021-11-20 22:41:11 +0200

    update work_with_drop-down-list.py

--- 2_selenium_methods/2.2_work_with_files_and_lists_and_js-scripts/work_with_drop-down-list.py
+++ 2_selenium_methods/2.2_work_with_files_and_lists_and_js-scripts/work_with_drop-down-list.py
@@ -7,3 +7,29 @@
 # 2. Посчитать сумму заданных чисел
 # 3. Выбрать в выпадающем списке значение равное расчитанной сумме
 # 4. Нажать кнопку "Submit"
+
+from selenium import webdriver
+from selenium.webdriver.support import select
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+import time
+
+link = 'http://suninjuly.github.io/selects1.html'
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+    
+    num1 = int(browser.find_element(By.ID, 'num1').text)
+    num2 = int(browser.find_element(By.ID, 'num2').text)
+    
+    
+    dropdown_list = Select(browser.find_element(By.ID, 'dropdown'))
+    dropdown_list.select_by_value(str(num1 + num2))
+    
+    submit_button = browser.find_element(By.CSS_SELECTOR, '.btn.btn-default')
+    submit_button.click()
+    
+finally:
+    time.sleep(5)
+    browser.quit()
