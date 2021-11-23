fb7087811fb7e9c66c96c78b9f389ff354d6462a
Vladislav Bolshakov <bolshakov.vladislav@gmail.com>
2021-11-23 23:44:14 +0200

    update exercise decriptions

--- 2_selenium_methods/2.4_wait_setting/about_exceptions.py
+++ 2_selenium_methods/2.4_wait_setting/about_exceptions.py
@@ -1,3 +1,7 @@
+# Задание:
+
+#Какую ошибку вы увидите в консоли, если попытаетесь выполнить команду browser.find_element_by_id("button") после открытия страницы http://suninjuly.github.io/cats.html?
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
